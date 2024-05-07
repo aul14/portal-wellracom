573cf1b99eeda8ce8d1c7a307ea0a28ad40f33ca
aul14 <rahmanaulia336@gmail.com>
2024-05-07 13:45:50 +0700

    update

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -13,6 +13,8 @@ import permissionsRoute from './routes/permissions.js';
 import permissionsRolesRoute from './routes/permissionsRoles.js';
 import refreshTokensRoute from './routes/refreshToken.js';
 
+import verifyToken from './middlewares/verifyToken.js';
+
 const app = express();
 
 app.use(cors({
@@ -27,12 +29,12 @@ app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(express.static("public"));
 
 app.use('/auth', authsRoute);
-app.use('/users', usersRoute);
-app.use('/roles', rolesRoute);
-app.use('/modules', modulesRoute);
-app.use('/permissions', permissionsRoute);
-app.use('/access-control', permissionsRolesRoute);
-app.use('/refresh-tokens', refreshTokensRoute);
+app.use('/users', verifyToken, usersRoute);
+app.use('/roles', verifyToken, rolesRoute);
+app.use('/modules', verifyToken, modulesRoute);
+app.use('/permissions', verifyToken, permissionsRoute);
+app.use('/access-control', verifyToken, permissionsRolesRoute);
+app.use('/refresh-tokens', verifyToken, refreshTokensRoute);
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server up and running...`);
